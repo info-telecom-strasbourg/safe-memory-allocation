3716df2b7a4163af23ed83dc30e9c34400d91d4f
Clara Schild <clara.schild@etu.unistra.fr>
2020-03-19 19:37:16 +0100

    feat: calloc macro

--- mememory-allocation.h
+++ mememory-allocation.h
@@ -7,4 +7,13 @@ void **__TAB_MEM = NULL;
 
 #define ACHECK(op) do { if ((op) == NULL) fprintf(stderr, "%s\n", #op); } while (0)
 
+#define _calloc(a,b) \
+({ \
+        __SIZE+=1;\
+	ACHECK(__TAB_MEM = realloc(__TAB_MEM, __SIZE * sizeof(void*))); \
+        ACHECK(__TAB_MEM[__SIZE-1] = calloc(a,b)); \
+	printf("%p\n", __TAB_MEM[__SIZE-1]); \
+        __TAB_MEM[__SIZE-1]; \
+})
+
 #endif
